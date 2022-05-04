f56237d026032c45679fdff78dd6c2fa90860a75
SangDon <tkdehs0454@naver.com>
2022-05-04 23:40:10 +0900

    model 생성
    model 생성

---
+++ MemoAppWithRxSwift/Memo.swift
@@ -0,0 +1,26 @@
+//
+//  Memo.swift
+//  MemoAppWithRxSwift
+//
+//  Created by PNX on 2022/05/04.
+//
+
+import Foundation
+
+struct Memo: Equatable {
+    var content: String
+    var insertDate: Date
+    var identity: String
+    
+    
+    init(content: String, insertDate: Date = Date()) {
+        self.content = content
+        self.insertDate = insertDate
+        self.identity = "\(insertDate.timeIntervalSinceReferenceDate)"
+    }
+    
+    init(original: Memo, updateContent: String) {
+        self = original
+        self.content = updateContent
+    }
+}
